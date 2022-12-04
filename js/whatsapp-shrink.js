7511f184bff6f9db1a451a99d9d6694f07f95f83
leomillan22 <leonardomillan08@gmail.com>
2022-12-04 10:16:13 -0300

    animacao btn wpp

--- js/whatsapp-shrink.js
+++ js/whatsapp-shrink.js
@@ -2,7 +2,12 @@ const content = document.querySelector('[data-shrink="yes"]');
 
 const span = document.querySelector('[data-shrink="yes"] span');
 
+span.classList.add('aparecer')
 setInterval(function(){
     content.classList.toggle('shrink')
+}, 5000)
+
+setInterval(function(){
+    span.classList.toggle('aparecer')
 }, 5000)
 
